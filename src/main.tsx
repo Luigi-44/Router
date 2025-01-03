5ef2f24665fde790ff3b5f938f84d42da89c73a5
Luigi-44 <luigi.rondeau@gmail.com>
2025-01-03 09:54:32 +0100

    exemple router

---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+
+// page components
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// router creation
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+// rendering
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+}
